8cc521af62867ada34f92375cf7d310793262588
Eliam Nahum <braca.eliam@gmail.com>
2025-04-08 18:58:09 -0700

    Agregue Api y varios cambios en otros archivos para recoger datos

---
+++ src/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const searchImagenes = async(term) => {
+    const url = 'https://api.unsplash.com/search/photos/?client_id=QJy_z4nyZ00VhBicju2u4PsFRTMTJTVmfKuYpBxJVEY'
+
+    const response = await axios.get(url, {
+        params:{
+            query: 'cars'
+        }
+    })
+
+    console.log(response)
+    return response.data.results
+}
+
+export default searchImagenes
